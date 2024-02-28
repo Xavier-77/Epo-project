9528c71703c2076343efe2b9e4b678c6ea928820
BADO Xavier <badoxavierzwamassoe@gmail.com>
2024-02-28 15:14:13 +0000

    Ajout d'une galerie d'images pour l'EPO

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ document.getElementById('welcomeButton').addEventListener('click', function() {
  document.getElementById('welcomeMessage').textContent = 'Nous sommes ravis de vous 
 accueillir sur le site dédié à l\'EPO. Explorez nos programmes et nos initiatives!';
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+ const images = document.querySelectorAll("#galerie img");
+ images.forEach(image => {
+ image.addEventListener("click", () => {
+ alert("Vous avez cliqué sur une image !");
+ });
+ });
+});
